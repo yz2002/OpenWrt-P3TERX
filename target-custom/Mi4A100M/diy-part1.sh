9ba3eda3443685a5496a99fecb83019277a41795
SolomonRicky2002 <55992481+yz2002@users.noreply.github.com>
2021-09-30 18:22:16 +0800

    Initial

---
+++ target-custom/Mi4A100M/diy-part1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i "s/@arm/@(arm||aarch64)/g" package/lean/luci-app-cpufreq/Makefile
+#sed -i "s/"services"/"system"/g" package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+#git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+#git clone https://github.com/garypang13/smartdns-le package/smartdns-le
+#git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
---
+++ target-custom/Xiaomi_AC2100/diy-part2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Enable Wi-Fi
+sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify SSID
+sed -i 's/wireless.default_radio${devidx}.ssid=OpenWrt/wireless.default_radio${devidx}.ssid=MiAC2100/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify TimeZone
+sed -i 's/system.@system[-1].timezone='UTC'/system.@system[-1].timezone='Kuala Lumpur'/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/MiAC2100/g' package/base-files/files/bin/config_generate
+
+# Add kernel build user
+sed -i 's/CONFIG_KERNEL_BUILD_USER=""/CONFIG_KERNEL_BUILD_USER="SolomonRicky"/g' .config
+
+# Add kernel build domain
+sed -i 's/CONFIG_KERNEL_BUILD_DOMAIN=""/CONFIG_KERNEL_BUILD_DOMAIN="yzyz.ga"/g' .config
+
+# Add build information
+echo -e '\nOpenWrt by SolomonRicky\n'  >> package/base-files/files/etc/banner
+sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+# echo "DISTRIB_REVISION='v21.02.0-rc3'" >> package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='OpenWrt by SolomonRicky $(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
+
+# Modify banner
+sed -i "s/OpenWrt/Mi AC2100's Openwrt/g" files/etc/banner
